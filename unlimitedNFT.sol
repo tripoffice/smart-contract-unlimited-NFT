09ddba6f398c9a22538687abc5790427f1b2ba33
Grzesiek Kowalski <79772462+grzesiek-kowalski@users.noreply.github.com>
2022-07-06 00:44:30 +0200

    Gas optimization

--- unlimitedNFT.sol
+++ unlimitedNFT.sol
@@ -7,13 +7,13 @@ import "erc721a/contracts/ERC721A.sol";
 import "erc721a/contracts/extensions/ERC721ABurnable.sol";
 import "erc721a/contracts/extensions/ERC721AQueryable.sol";
 
-contract DNCT2 is Ownable, ERC721A, ERC721ABurnable, ERC721AQueryable {
+contract DNC3 is Ownable, ERC721A, ERC721ABurnable, ERC721AQueryable {
 
     string public tokenUri = "https://nft.trvcdn.com/dnc/"; // Initial base URI
     uint256 public currentMaxSupply = 500;
     uint256 public currentPriceWei = 10000000000000000000; // 10USD
 
-    IERC20[] public stablecoins = [
+    IERC20[16] public stablecoins = [
         IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48), // USDC Ethereum
         IERC20(0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174), // USDC Polygon
         IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7), // USDT Ethereum
@@ -25,45 +25,48 @@ contract DNCT2 is Ownable, ERC721A, ERC721ABurnable, ERC721AQueryable {
 
     constructor() ERC721A("Digital Nomads Club", "DNC") {}
 
-    function addCurrency(IERC20 paytoken) public onlyOwner {
-        stablecoins.push(paytoken);
+    function setActiveMintUSD(bool active) external onlyOwner {
+        activeMintUSD = active;
     }
 
-    function removeCurrency(uint256 i) public onlyOwner {
+    function setActiveMintETH(bool active) external onlyOwner {
+        activeMintETH = active;
+    }
+
+    function addCurrency(uint i, IERC20 paytoken) external onlyOwner {
+        stablecoins[i] = paytoken;
+    }
+
+    function removeCurrency(uint i) external onlyOwner {
         delete stablecoins[i];
     }
 
-    function changeBaseUri(string memory newUri) public onlyOwner {
+    function changeBaseUri(string calldata newUri) external onlyOwner {
         tokenUri = newUri;
     }
 
-    function increaseMaxSupplyAndPrice(uint256 newMaxSupply, uint256 newPriceWei) public onlyOwner {
+    function increaseMaxSupplyAndPrice(uint256 newMaxSupply, uint256 newPriceWei) external onlyOwner {
         currentMaxSupply = newMaxSupply;
         currentPriceWei = newPriceWei;
     }
 
     function publicMintUSD(address to, uint256 quantity, uint256 pid) external payable {
-        require(activeMintUSD, "Mint is not active");
-        require(totalSupply() + quantity <= currentMaxSupply, "Cannot exceed current total supply");
-        require(msg.value == quantity * currentPriceWei, "Not enough balance to complete transaction");
-        IERC20 paytoken = stablecoins[pid];
-        paytoken.transferFrom(msg.sender, address(this), quantity * currentPriceWei);
+        uint256 totalPrice = quantity * currentPriceWei;
+        require(activeMintUSD && totalSupply() + quantity <= currentMaxSupply && msg.value == totalPrice);
+        stablecoins[pid].transferFrom(msg.sender, address(this), totalPrice);
         _mint(to, quantity);
     }
 
     function publicMintETH(address to, uint256 quantity) external payable {
-        require(activeMintETH, "Mint is not active");
-        require(totalSupply() + quantity <= currentMaxSupply, "Cannot exceed current total supply");
-        require(msg.value == quantity * currentPriceWei, "Not enough eth for mint");
+        require(activeMintETH && totalSupply() + quantity <= currentMaxSupply && msg.value == quantity * currentPriceWei);
         _mint(to, quantity);
     }
 
-    function withdrawUSD(address payable to, uint256 pid) public onlyOwner() {
-        IERC20 paytoken = stablecoins[pid];
-        paytoken.transfer(to, paytoken.balanceOf(address(this)));
+    function withdrawUSD(address payable to, uint256 pid) external payable onlyOwner() {
+        stablecoins[pid].transfer(to, paytoken.balanceOf(address(this)));
     }
 
-    function withdrawETH(address payable to) public onlyOwner {
+    function withdrawETH(address payable to) external onlyOwner {
         to.transfer(address(this).balance);
     }
 }
