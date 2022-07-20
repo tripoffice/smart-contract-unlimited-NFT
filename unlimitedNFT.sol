845f320319e411c3c06fe823b2b290dbd1982c4b
Grzesiek Kowalski <79772462+grzesiek-kowalski@users.noreply.github.com>
2022-07-20 21:22:03 +0200

    v3

--- unlimitedNFT.sol
+++ unlimitedNFT.sol
@@ -1,71 +1,59 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DNCPOL is ERC721Enumerable, ERC721URIStorage, Ownable {
 
-    string public tokenUri = "https://nft.trvcdn.com/dnc/"; // Initial base URI
+contract DNCT is ERC721Enumerable, Ownable {
 
-    uint256 public counter = 0;
-    uint256 public currentMaxSupply = 500;
-    uint256 public currentPriceWei = 10000000000000000000; // 10USD
+    string public tokenUri = "https://nft.trvcdn.com/dnc/info/"; // Initial base URI
 
-    IERC20[16] public stablecoins = [
-        IERC20(0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174), // USDC Polygon
-        IERC20(0xc2132D05D31c914a87C6611C10748AEb04B58e8F),  // USDT Polygon
-    ];
+    uint256 public counter;
+    uint256 public maxSupply;
+    uint256 public priceMatic;
+    uint256 public priceUSD;
 
-    bool public activeMintUSD = true;
-    bool public activeMintETH = false;
+    IERC20 public stablecoin = IERC20(0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174); // USDC Polygon
+
+    bool public activeMatic = false;
+    bool public activeUSD = false;
 
     constructor() ERC721("Digital Nomads Club", "DNC") {}
 
-    function setActiveMintUSD(bool active) external onlyOwner {
-        activeMintUSD = active;
+    function setActiveMatic(bool active) external onlyOwner {
+        activeMatic = active;
     }
 
-    function setActiveMintETH(bool active) external onlyOwner {
-        activeMintETH = active;
+    function setActiveUSD(bool active) external onlyOwner {
+        activeUSD = active;
     }
 
-    function addCurrency(uint i, IERC20 paytoken) external onlyOwner {
-        stablecoins[i] = paytoken;
+    function changeStablecoin(IERC20 newStablecoin) external onlyOwner {
+        stablecoin = newStablecoin;
     }
 
-    function removeCurrency(uint i) external onlyOwner {
-        delete stablecoins[i];
-    }
-
-    function changeBaseUri(string calldata newUri) external onlyOwner {
+    function changeTokenUri(string calldata newUri) external onlyOwner {
         tokenUri = newUri;
     }
 
-    function increaseMaxSupplyAndPrice(uint256 newMaxSupply, uint256 newPriceWei) external onlyOwner {
-        currentMaxSupply = newMaxSupply;
-        currentPriceWei = newPriceWei;
+    function changePriceMatic(uint256 newPriceMatic) external onlyOwner {
+        priceMatic = newPriceMatic;
+    }
+
+    function changePriceUSD(uint256 newPriceUSD) external onlyOwner {
+        priceUSD = newPriceUSD;
+    }
+
+    function changeMaxSupply(uint256 newMaxSupply) external onlyOwner {
+        maxSupply = newMaxSupply;
     }
 
     function fixCounter(uint256 c) external onlyOwner {
         counter = c;
     }
 
-    function publicMintUSD(address to, uint256 quantity, uint256 pid) external payable {
-        uint256 totalPrice = quantity * currentPriceWei;
-        require(activeMintUSD && counter + quantity <= currentMaxSupply && msg.value == totalPrice);
-        stablecoins[pid].transferFrom(msg.sender, address(this), totalPrice);
-        uint256 tmpCounter = counter;
-        counter += quantity;
-        for(uint256 i = 1; i <= quantity; i++) {
-            _mint(to, tmpCounter + i);
-        }        
-    }
-
-    function publicMintETH(address to, uint256 quantity) external payable {
-        require(activeMintETH && counter + quantity <= currentMaxSupply && msg.value == quantity * currentPriceWei);
+    function privateMint(address to, uint256 quantity) external onlyOwner {
         uint256 tmpCounter = counter;
         counter += quantity;
         for(uint256 i = 1; i <= quantity; i++) {
@@ -73,30 +61,47 @@ contract DNCPOL is ERC721Enumerable, ERC721URIStorage, Ownable {
         }
     }
 
-    function withdrawUSD(address payable to, uint256 pid) external payable onlyOwner() {
-        IERC20 paytoken = stablecoins[pid];
-        paytoken.transfer(to, paytoken.balanceOf(address(this)));
+    function publicMintUSD(address to, uint256 quantity) external {
+        uint256 totalPrice = quantity * priceUSD;
+        require(activeUSD, "Public sale not active (USD)!");
+        require(counter + quantity <= maxSupply, "Cannot mint more than current max supply!");
+        require(stablecoin.transferFrom(msg.sender, address(this), totalPrice), "Cannot send stablecoins!");
+        uint256 tmpCounter = counter;
+        counter += quantity;
+        for(uint256 i = 1; i <= quantity; i++) {
+            _mint(to, tmpCounter + i);
+        }
     }
 
-    function withdrawETH(address payable to) external onlyOwner {
+    function publicMintMatic(address to, uint256 quantity) external payable {
+        require(activeMatic, "Public sale not active (matic)!");
+        require(counter + quantity <= maxSupply, "Cannot mint more than current max supply!");
+        require(msg.value == quantity * priceMatic, "Incorrect amount sent!");
+        uint256 tmpCounter = counter;
+        counter += quantity;
+        for(uint256 i = 1; i <= quantity; i++) {
+            _mint(to, tmpCounter + i);
+        }
+    }
+
+    function tokensOfOwner(address owner) external view returns (uint256[] memory) {
+        uint256 tokenCount = balanceOf(owner);
+        uint256[] memory tokensId = new uint256[](tokenCount);
+        for (uint256 i = 0; i < tokenCount; i++) {
+            tokensId[i] = tokenOfOwnerByIndex(owner, i);
+        }
+        return tokensId;
+    }
+
+    function withdrawUSD(address payable to) external onlyOwner {
+        stablecoin.transfer(to, stablecoin.balanceOf(address(this)));
+    }
+
+    function withdrawMatic(address payable to) external payable onlyOwner {
         to.transfer(address(this).balance);
     }
 
-    // The following functions are overrides required by Solidity.
-
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
-
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-        return super.tokenURI(tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
-        return super.supportsInterface(interfaceId);
+    function _baseURI() internal view override returns (string memory) {
+        return tokenUri;
     }
 }
